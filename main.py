786b2d95c67050ded35452e5c887c604f8376438
Maiykame <maiykame@gmail.com>
2025-06-07 14:12:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True  # Wichtig für Befehle ab API v2
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot ist eingeloggt als {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+token = os.getenv("TOKEN")
+if token is None:
+    print("Fehlender TOKEN in Umgebungsvariablen")
+else:
+    bot.run(TOKEN)
