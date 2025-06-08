9035f51b68fc40bd96aa5c515c9d38eec9b0b372
Maiykame <maiykame@gmail.com>
2025-06-08 11:09:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), help_comma
 @bot.event
 async def on_ready():
     print(f"Bot ist eingeloggt als {bot.user}")
-    await create_reaction_message(1381197888985763910, {
+    await create_reaction_message(1381198483746324572, {
         "💻": 1364631941307043972, # PC
         "🎮": 1364631972621844541, # Playstation
         "🟢": 1364632015042908180, # XBox
