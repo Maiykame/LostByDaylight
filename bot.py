00e90c03f76df67099fedad2ed0cba16e45b568d
Maiykame <maiykame@gmail.com>
2025-06-12 00:47:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 import requests
 
+from Misc.on_ready import on_ready_event
+
 # Globale Mapping-Tabelle für Reaction Roles
 reaction_role_messages = {}
 
@@ -10,17 +12,7 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), help_comma
 
 @bot.event
 async def on_ready():
-    print(f"Bot ist eingeloggt als {bot.user}")
-    await create_reaction_message(1381198483746324572, {
-        "💻": 1364631941307043972, # PC
-        "🎮": 1364631972621844541, # Playstation
-        "🟢": 1364632015042908180, # XBox
-        "🔴": 1364632054335144056# Switch
-    })
-    await bot.change_presence(
-        status=discord.Status.online,
-        activity=discord.Game(name='!help')
-    )
+    on_ready_event(bot)
 
 @bot.command()
 async def help(ctx):
