efe5b6116c8a1467f1bd6dc4460a0ae8ebc1a871
Maiykame <maiykame@gmail.com>
2025-06-11 17:14:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,6 +94,12 @@ async def sudo(ctx, *, message):
         pass  # Bot darf die Nachricht nicht löschen
     await ctx.send(message)
 
+@bot.command()
+@commands.has_permission(adminstrator=True)
+async def spam_ping(ctx, id: int, amount: int):
+    for i in range(amount):
+        await ctx.send(f"<@{id}> kommt Online!!!")
+
 token = os.getenv("TOKEN")
 if token is None:
     print("Fehlender TOKEN in Umgebungsvariablen")
