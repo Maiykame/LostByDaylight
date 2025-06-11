332e9b6b05f487dbee1fd62e9f66b526fa4faf52
Maiykame <maiykame@gmail.com>
2025-06-12 01:19:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from Event.on_raw_reaction_add import on_raw_reaction_add_event
 from Event.on_raw_reaction_remove import on_raw_reaction_remove_event
 
 from Commands.help import help_command
+from Commands.shrine import shrine_command
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), help_command=None)
 
@@ -29,25 +30,7 @@ async def on_raw_reaction_remove(payload):
 
 @bot.command()
 async def shrine(ctx):
-    url = "https://api.nightlight.gg/v1/shrine?pretty=true"
-    res = requests.get(url)
-
-    if res.status_code != 200:
-        await ctx.send(f"❌ Fehler beim Abrufen der Shrine-Daten. Status-Code: {res.status_code}")
-        return
-
-    text = res.text
-    perks_part = text.split('|')[0].strip()
-    perks = [p.strip() for p in perks_part.split(',')]
-
-    embed = discord.Embed(
-        title="🛐 Aktuelle Shrine of Secrets",
-        description="\n".join(f"- {perk}" for perk in perks),
-        color=discord.Color.dark_red()
-    )
-    embed.set_footer(text="Daten von nightlight.gg")
-
-    await ctx.send(embed=embed)
+    await shrine_command(ctx)
 
 
 @bot.command()
