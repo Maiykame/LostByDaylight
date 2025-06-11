d8273501bc1a3b67a18ac5d0a5d400e6d8a2eb2a
Maiykame <maiykame@gmail.com>
2025-06-12 01:10:28 +0200

    Create on_raw_reaction_add.py

---
+++ Event/on_raw_reaction_add.py
@@ -0,0 +1,17 @@
+
+async def on_raw_reaction_add_event(bot, payload):
+    if payload.message_id not in reaction_role_messages:
+        return
+
+    emoji_map = reaction_role_messages[payload.message_id]
+    role_id = emoji_map.get(str(payload.emoji))
+    if not role_id:
+        return
+
+    guild = bot.get_guild(payload.guild_id)
+    member = guild.get_member(payload.user_id)
+    role = guild.get_role(role_id)
+
+    if guild and member and role and not member.bot:
+        await member.add_roles(role)
+        print(f"✅ Rolle {role.name} zugewiesen an {member.name}")
