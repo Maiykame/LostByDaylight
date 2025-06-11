2f9f3556e445c161acc3c582b2306e964a9e5fc4
Maiykame <maiykame@gmail.com>
2025-06-12 01:17:00 +0200

    Update on_raw_reaction_remove.py

--- Event/on_raw_reaction_remove.py
+++ Event/on_raw_reaction_remove.py
@@ -1,1 +1,18 @@
+from Misc.create_reaction_message import reaction_role_messages
 
+async def on_raw_reaction_remove(bot, payload):
+    if payload.message_id not in reaction_role_messages:
+        return
+
+    emoji_map = reaction_role_messages[payload.message_id]
+    role_id = emoji_map.get(str(payload.emoji))
+    if not role_id:
+        return
+
+    guild = bot.get_guild(payload.guild_id)
+    member = guild.get_member(payload.user_id)
+    role = guild.get_role(role_id)
+
+    if guild and member and role and not member.bot:
+        await member.remove_roles(role)
+        print(f"❌ Rolle {role.name} entfernt von {member.name}")
