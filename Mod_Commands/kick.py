fc46a033e10282058b9abcd85ca4fff3dfb7f8f4
Maiykame <maiykame@gmail.com>
2025-06-12 02:04:19 +0200

    Create kick.py

---
+++ Mod_Commands/kick.py
@@ -0,0 +1,5 @@
+async def kick(ctx, member: discord.Member, *, reason="Kein Grund angegeben"):
+    if not member.kickable:
+        return await ctx.send("Ich kann diesen Benutzer nicht kicken.")
+    await member.kick(reason=reason)
+    await ctx.send(f"{member.mention} wurde gekickt. Grund: {reason}")
