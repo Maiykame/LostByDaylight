742641773e294a6caaa7c58c9a84dc4498242a21
Maiykame <maiykame@gmail.com>
2025-06-12 02:10:23 +0200

    Delete Mod_Commands directory

--- Mod_Commands/kick.py
+++
@@ -1,5 +0,0 @@
-async def kick(ctx, member, reason="Kein Grund angegeben"):
-    if not member.kickable:
-        return await ctx.send("Ich kann diesen Benutzer nicht kicken.")
-    await member.kick(reason=reason)
-    await ctx.send(f"{member.mention} wurde gekickt. Grund: {reason}")
