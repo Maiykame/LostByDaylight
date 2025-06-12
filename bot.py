58d948fad03b1920c7d067ae5d8185ab9fa6f980
Maiykame <maiykame@gmail.com>
2025-06-12 02:05:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ async def sudo(ctx, message):
 @bot.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason="Kein Grund angegeben"):
-    kick_command(ctx, member: discord.Member, *, reason="Kein Grund angegeben")
+    kick_command(ctx, member, *, reason)
 
 token = os.getenv("TOKEN")
 if token is None:
